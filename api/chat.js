32646fb92a597f710f01f67d3f24c4f7abd4b204
kleberdiasFecaf <kleber.dias@pro.fecaf.com.br>
2025-10-21 19:15:31 -0300

    Corrigindo crash no backend (api/chat.js) 2

--- api/chat.js
+++ api/chat.js
@@ -29,7 +29,8 @@ export default async function handler(req, res) {
         }
 
         // Inicializa o modelo Gemini
-        const model = genAI.getGenerativeModel({ model: "gemini-pro" });
+        //const model = genAI.getGenerativeModel({ model: "gemini-pro" });
+        const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
 
         // Gera o conteúdo
         const result = await model.generateContent(prompt);
